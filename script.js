b9113d4de4fbdcf803849c57f92e829517e1f686
jpoeep <jpoe.ep@mysummitps.org>
2017-07-19 18:11:33 -0700

    Update script.js
    trying a new thing with this request stuff. for now i am changing the name of the constructor to "Requester" because to be honest, i am not sure how much this constructor really contributes to APIs. i am trying a new approach to connecting to different branches within the "base" URL. each new branch is now defined by creating a new object and setting the appropriate properties. instead of having a singular XMLHttpRequest existing, each branch object gets its own XMLHttpRequest which means that while for all i know performance may not be as good, requests can happen simultaneously. if you want a callback that waits for every request to be ready, you can use onready so every response is ready before anything happens. i've temporarily removed the lines of code that involve HTTP headers and REST

--- script.js
+++ script.js
@@ -1,53 +1,106 @@
-function JSONP(param) {
-  if(typeof param == "object") {
-    var entries = []
-    var entryList = Object.entries(param)
+function REST(param) {
+    if(typeof param == "object") {
+        var entries = []
+        var entryList = Object.entries(param)
 
-    entryList.forEach(function(ent) {
-      var suffix = ent[1]
+        entryList.forEach(function(ent) {
+        var suffix = ent[1]
 
-      if(entryList.indexOf(ent) < entryList.length - 1) {
-        suffix += "&"
-      }
+        if(entryList.indexOf(ent) < entryList.length - 1) {
+            suffix += "&"
+        }
 
-      entries.push(ent[0] + "=" + suffix)
-    })
+        entries.push(ent[0] + "=" + suffix)
+        })
 
-    return "?" + entries.join("")
-  }
-
-  else {
-    return ""
-  }
-}
-
-function APIRequest(base) {
-  var _this = this
-  var xhr = new XMLHttpRequest()
-
-  this.connect = function(branch, obj) {
-    if(!branch) {
-      branch = ""
+        return "?" + entries.join("")
     }
 
     else {
-      obj = {json: ""}
+        return ""
     }
-
-    xhr.open(_this.method, base + branch + JSONP(obj.json), true)
-
-    if(obj.headers) {
-      Object.entries(obj.headers).forEach(function(ent) {
-        xhr.setRequestHeader(ent[0], ent[1])
-      })
-    }
-
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState == 4) {
-        _this.callback(xhr.response)
-      }
-    }
-
-    xhr.send()
-  }
 }
+
+function Requester(base, cfg) {
+    var _this = this
+    //var xhr = new XMLHttpRequest()
+    this.readyRequests = []
+    
+    this.connect = function(branch) {
+        var target
+
+        if(!branch || branch == "") {
+            cfg.path = base
+            target = cfg
+        }
+
+        else if(branch) {
+            target = branch
+        }
+
+        if(!target.xhr) {
+            target.xhr = new XMLHttpRequest()
+        }
+
+        target.xhr.open(target.method, base + target.path)
+        target.xhr.responseType = target.type
+
+        target.xhr.onreadystatechange = function() {
+            if(target.xhr.readyState == 4) {
+                if(target.callback) {
+                    target.callback(target.xhr.response)
+                }
+
+                _this.readyRequests.push(target)
+
+                var requests = []
+
+                if(_this.xhr) {
+                    requests.push(_this)
+                }
+
+                for(let i in _this) {
+                    if(_this[i].xhr) {
+                        requests.push(_this[i])
+                    }
+                }
+
+                if(_this.readyRequests.length == requests.length) {
+                    _this.onready()
+                }
+            }
+        }
+
+        target.xhr.send()
+    }
+}
+
+/* example code
+var html5 = new Requester("https://www.html5rocks.com")
+
+html5.en = {
+  path: "/en/",
+  method: "GET",
+  type: "document"
+}
+
+html5.tutorial = {
+  path: "/en/tutorials/",
+  method: "GET",
+  type: "document"
+}
+
+html5.hobbit = {
+  path: "/en/tutorials/casestudies/hobbit2014/",
+  method: "GET",
+  type: "document"
+}
+
+html5.connect(html5.en)
+html5.connect(html5.tutorial)
+html5.connect(html5.hobbit)
+
+html5.onready = function() {
+  console.log(html5.en.xhr.response, html5.tutorial.xhr.response, html5.thirdPage.xhr.response)
+}
+*/
